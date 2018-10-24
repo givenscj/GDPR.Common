0a3f5cb5f05b06cf35949ffc719e364bd074c6b3
Chris Givens <chris@solliance.net>
2018-10-24 12:46:47 -0700

    more movement

---
+++ Processor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPR.Common
+{
+    public partial class Processor
+    {
+        public System.Guid ProcessorId { get; set; }
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public string ProcessorClass { get; set; }
+        public System.DateTime ModifyDate { get; set; }
+        public System.DateTime CreateDate { get; set; }
+        public string ApiEndPoint { get; set; }
+        public byte[] PublicKey { get; set; }
+    }
+}
---
+++ RecordCollection.cs
@@ -0,0 +1,34 @@
+﻿using GDPR.Common.Classes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPR.Util.Classes
+{
+    public class RecordCollection
+    {
+        private List<Record> records = new List<Record>();
+
+        private Hashtable keys = new Hashtable();
+
+        public void AddRecord(Record r)
+        {
+            if (!keys.ContainsKey(r.Type + r.RecordId))
+            {
+                keys.Add(r.Type + r.RecordId, r.Type + r.RecordId);
+                records.Add(r);
+            }
+        }
+
+        public List<Record> Records
+        {
+            get
+            {
+                return this.records;
+            }
+        }
+    }
+}
