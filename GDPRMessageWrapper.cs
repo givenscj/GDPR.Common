23809b23ff484973501c0fc03a57856f81d51e0f
Chris Givens <chris@solliance.net>
2018-10-24 11:53:50 -0700

    movements

---
+++ GDPRMessageWrapper.cs
@@ -0,0 +1,24 @@
+﻿using GDPR.Util.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPR.Util.Messages
+{
+    public class GDPRMessageWrapper
+    {
+        public Processor Source { get; set; }
+        public string ApplicationId { get; set; }
+        public string Authentication { get; set; }
+        public string Type { get; set; }
+        public int Retries { get; set; }
+        public bool IsError { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Object { get; set; }
+        public string OffSet { get; set; }
+        public DateTime MessageDate { get; set; }
+        public DateTime OriginalMessageDate { get; set; }
+    }
+}
