653eda4b3706eff23f64b277246970568a2c57c5
Chris Givens <givenscj@Hotmail.com>
2019-02-01 09:09:58 -0800

    Update BaseQueryMessage.cs

--- Messages/BaseQueryMessage.cs
+++ Messages/BaseQueryMessage.cs
@@ -1,11 +1,4 @@
 ﻿namespace GDPR.Common.Messages
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GDPR.Common.Messages
 {
     public class BaseQueryMessage : BaseApplicationMessage
     {
