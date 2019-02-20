1c00e64a34629131ae3e48bc20cebec4f05d7fda
Chris Givens <givenscj@Hotmail.com>
2019-02-19 18:21:32 -0800

    Update Utility.cs

--- Utility.cs
+++ Utility.cs
@@ -246,6 +246,23 @@ namespace GDPR.Common
 
             return "";
         }
+
+        public static string CalculateMD5Hash(string input)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
         public static string SerializeObject(object o, int depth)
         {
             //trim the request down...
